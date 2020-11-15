0725fdcba9273180609c2f911ef27635a569b5a0
TruptiSahu <truptiranjansahu2@gmail.com>
2020-11-15 07:51:30 -0800

    Bug fix using normal for loop

--- js/main.js
+++ js/main.js
@@ -3,9 +3,18 @@ const sliderVideos = document.querySelectorAll('.slider__slide');
 
 let i = 0;
 
-for (sliderVideo of sliderVideos) {
-  sliderVideo.addEventListener('click', () => {
+//! Error not able to get the index properly
+// for (sliderVideo of sliderVideos) {
+//   sliderVideo.addEventListener('click', () => {
+//     video.src = `../video/${i + 1}.mp4`;
+//     i++;
+//     console.log(i);
+//   });
+// }
+
+for (let i = 0; i < sliderVideos.length; i++) {
+  sliderVideos[i].addEventListener('click', () => {
     video.src = `../video/${i + 1}.mp4`;
-    i++;
   });
+  console.log(i);
 }
